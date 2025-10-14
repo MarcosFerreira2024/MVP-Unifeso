a2ecfc80365a4fd3bc6bd8f3c241d8c831371766
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-14 18:17:08 -0300

    Criando o VO de senha

---
+++ src/domain/value objects/Password.ts
@@ -0,0 +1,18 @@
+import { validateValueOrThrow } from "../../helpers/validateValueOrThrow";
+
+class Password {
+  private static readonly regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  private static readonly validationErrorMessage =
+    "Senha inválida: deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial (@$!%*?&).";
+
+  static validatePassword(password: string) {
+    return validateValueOrThrow(
+      password,
+      this.regex,
+      this.validationErrorMessage
+    );
+  }
+}
+
+export { Password };
