c5584aa33de867a7afbe8bc1bf080aa032ae80d8
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-15 05:28:00 -0300

    Criando a interface do Repositorio de Usuario

---
+++ src/domain/interfaces/IUserRepository.ts
@@ -0,0 +1,25 @@
+import {
+  UserFromDB,
+  UserFromDBWithRelations,
+} from "../../infrastructure/types/dataBase";
+import { User } from "../entities/User";
+
+export type Role = "USER" | "ADMIN";
+
+export type UpdateUserDTO = {
+  name?: string;
+  email?: string;
+  password?: string;
+  verified?: boolean;
+  role?: Role;
+};
+
+interface IUserRepository {
+  create(user: User): Promise<void>;
+  findByEmail(email: string): Promise<UserFromDBWithRelations | null>;
+  findById(id: string): Promise<UserFromDBWithRelations | null>;
+  update(userId: string, data: UpdateUserDTO): Promise<UserFromDB>;
+  delete(userId: string): Promise<void>;
+}
+
+export { IUserRepository };
