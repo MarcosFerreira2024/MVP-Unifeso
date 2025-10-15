a773f80214e55b783068eb698914de755f1b4d88
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-15 05:31:19 -0300

    Create enums.ts

---
+++ src/infrastructure/types/enums.ts
@@ -0,0 +1,16 @@
+export enum Roles {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+export enum Providers {
+  GOOGLE = "GOOGLE",
+}
+
+export enum RatingValues {
+  ONE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+}
