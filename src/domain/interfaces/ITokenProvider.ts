8c336948d0f0178952953f865024df6e0581c390
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-15 05:27:47 -0300

    Criando a interface de TokenProvider

---
+++ src/domain/interfaces/ITokenProvider.ts
@@ -0,0 +1,6 @@
+interface ITokenProvider {
+  generate(userId: string, email: string, role: string): Promise<string>;
+  verify(
+    token: string
+  ): Promise<{ userId: string; role: string; email: string } | null>;
+}
