4214a55d497524b8d0966d5ce1da44e36ab6b761
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:38:57 -0300

    Resolvendo caminho

--- src/infrastructure/services/email/EmailService.ts
+++ src/infrastructure/services/email/EmailService.ts
@@ -1,26 +1,33 @@
+import { injectable } from "tsyringe";
 import { transporter } from "../../libs/nodemailer/config";
 
 import fs from "fs/promises";
+import path from "path";
 
+@injectable()
 class EmailService implements IEmailService {
   async sendVerificationCode(
     name: string,
     email: string,
     code: string
   ): Promise<void> {
-    const template = await fs.readFile(
-      "./email/template/verificationCode.html",
-      "utf-8"
+    const templatePath = path.join(
+      process.cwd(),
+      "src/infrastructure/services/email/template/verificationCode.html"
     );
+    const template = await fs.readFile(templatePath, "utf-8");
+
     const html = template.replace("{{name}}", name).replace("{{code}}", code);
 
-    transporter.sendMail({
-      from: "marcosfp2021@hotmail.com",
+    await transporter.sendMail({
+      from: "Terê Verde Online <marcosfp2787@gmail.com>",
+      replyTo: "marcosfp2021@hotmail.com",
       encoding: "utf-8",
       to: email,
       subject: "Verification code",
       html,
     });
+    return;
   }
 }
 
