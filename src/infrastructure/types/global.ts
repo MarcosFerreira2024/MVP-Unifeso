cf45a28552da1cba97ade6342ff1f0382bb6da36
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:39:31 -0300

    Criando um type para o output padrão de resposta

---
+++ src/infrastructure/types/global.ts
@@ -0,0 +1,10 @@
+export type UseCaseResponse<T = unknown> = {
+  status:
+    | "success"
+    | "error"
+    | "verification_required"
+    | "unauthorized"
+    | "login_required";
+  message: string;
+  data?: T;
+};
