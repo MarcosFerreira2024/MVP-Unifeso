591a9bdad25bbaad270eb96cc7ed31bbac81b3cb
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:36:07 -0300

    Criando novas actions para Verification

--- src/domain/interfaces/IVerificationCodeRepository.ts
+++ src/domain/interfaces/IVerificationCodeRepository.ts
@@ -1,15 +1,17 @@
-type VerificationCode = {
+export type VerificationCodeFromDB = {
   id: string;
   code: string;
   userId: string;
+  validUntil: Date;
   used: boolean;
   createdAt: Date;
   updatedAt: Date;
 };
 
 interface IVerificationCodeRepository {
-  create(): Promise<string>;
-  findByUserId(userId: string): Promise<VerificationCode | null>;
+  create(userId: string): Promise<string>;
+  findByUserId(userId: string): Promise<VerificationCodeFromDB | null>;
+  findCode(code: string): Promise<VerificationCodeFromDB | null>;
   delete(userId: string): Promise<void>;
 }
 
