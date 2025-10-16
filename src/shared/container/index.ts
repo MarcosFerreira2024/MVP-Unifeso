b083943545c33283aa7beab4060a9e6584e57983
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:41:23 -0300

    Container do Tsyringe para injeção de dependências

---
+++ src/shared/container/index.ts
@@ -0,0 +1,34 @@
+import { container } from "tsyringe";
+
+import { IUserRepository } from "../../domain/interfaces/IUserRepository";
+import { UserRepository } from "../../infrastructure/repositories/UserRepository";
+
+import { HashProvider } from "../../infrastructure/provider/HashProvider";
+
+import { TokenProvider } from "../../infrastructure/provider/TokenProvider";
+
+import { IVerificationCodeRepository } from "../../domain/interfaces/IVerificationCodeRepository";
+import { VerificationCodeRepository } from "../../infrastructure/repositories/VerificationCodeRepository";
+
+import { EmailService } from "../../infrastructure/services/email/EmailService";
+
+import { IVerificationCodeService } from "../../domain/interfaces/IVerificationCodeService";
+import { VerificationCodeService } from "../../infrastructure/services/verificationCode/VerificationCodeService";
+
+container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+
+container.registerSingleton<IHashProvider>("HashProvider", HashProvider);
+
+container.registerSingleton<ITokenProvider>("TokenProvider", TokenProvider);
+
+container.registerSingleton<IVerificationCodeRepository>(
+  "VerificationCodeRepository",
+  VerificationCodeRepository
+);
+
+container.registerSingleton<IEmailService>("EmailService", EmailService);
+
+container.registerSingleton<IVerificationCodeService>(
+  "VerificationCodeService",
+  VerificationCodeService
+);
