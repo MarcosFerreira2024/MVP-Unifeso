4805e4e0e6aa0b2fe2cc0f91e99716312d2ebdb1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:35:44 -0300

    Update ITokenProvider.ts

--- src/domain/interfaces/ITokenProvider.ts
+++ src/domain/interfaces/ITokenProvider.ts
@@ -2,5 +2,10 @@ interface ITokenProvider {
   generate(userId: string, email: string, role: string): Promise<string>;
   verify(
     token: string
-  ): Promise<{ userId: string; role: string; email: string } | null>;
+  ): Promise<{
+    userId: string;
+    role: string;
+    email: string;
+    token: string;
+  } | null>;
 }
