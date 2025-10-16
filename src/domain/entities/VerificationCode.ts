a2294c7ba74e8f5cc46b6c1d801d1c84d95c508e
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:35:41 -0300

    Update VerificationCode.ts

--- src/domain/entities/VerificationCode.ts
+++ src/domain/entities/VerificationCode.ts
@@ -1,25 +1,21 @@
 class VerificationCode {
   private readonly VERIFICATION_CODE_VALIDITY_MINUTES = 60;
   constructor(
-    private readonly userId: string,
     private readonly code: string,
-    private readonly createdAt: Date,
+    private readonly validUntil: Date,
+
     private used = false
   ) {}
 
-  public validate(inputCode: string, userId: string) {
+  public validate(inputCode: string) {
     if (this.used) throw new Error("Código já utilizado");
     if (this.isExpired()) throw new Error("Código expirado");
-    if (this.userId !== userId)
-      throw new Error("Código não pertence a este usuário");
     if (this.code !== inputCode) throw new Error("Código inválido");
-
-    this.used = true;
   }
 
   private isExpired() {
     const diffMinutes =
-      (new Date().getTime() - this.createdAt.getTime()) / 60000;
+      (this.validUntil.getTime() - new Date().getTime()) / 1000 / 60;
     return diffMinutes > this.VERIFICATION_CODE_VALIDITY_MINUTES;
   }
 }
