9be52df52e22f9fc7ece2a0fbeeeab96eada7af6
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-10-16 03:35:13 -0300

    Injetando dependências , finalizando SignUp

--- src/application/useCases/Authentication/SignUpUseCase.ts
+++ src/application/useCases/Authentication/SignUpUseCase.ts
@@ -1,32 +1,47 @@
+import { inject, injectable } from "tsyringe";
 import { User } from "../../../domain/entities/User";
 import { IUserRepository } from "../../../domain/interfaces/IUserRepository";
 import { IVerificationCodeRepository } from "../../../domain/interfaces/IVerificationCodeRepository";
 import { Password } from "../../../domain/value objects/Password";
-import { Roles } from "../../../infrastructure/types/enums";
+import { UseCaseResponse } from "../../../infrastructure/types/global";
 
+@injectable()
 class SignUpUseCase {
   constructor(
-    private userRepository: IUserRepository,
-    private hashProvider: IHashProvider,
+    @inject("UserRepository") private userRepository: IUserRepository,
+    @inject("HashProvider") private hashProvider: IHashProvider,
+    @inject("VerificationCodeRepository")
     private verificationCodeRepository: IVerificationCodeRepository,
-    private emailService: IEmailService
+    @inject("EmailService") private emailService: IEmailService
   ) {}
 
-  async execute(name: string, email: string, password: string): Promise<void> {
-    const alreadyExists = await this.userRepository.findByEmail(email);
-    if (alreadyExists) throw new Error("User already exists");
+  async execute(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<UseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email);
+    if (user) throw new Error("Usuario já cadastrado");
 
     const verifiedPassword = Password.validate(password);
-
-    const code = await this.verificationCodeRepository.create();
-    await this.emailService.sendVerificationCode(email, code);
 
     const hashedPassword = await this.hashProvider.generate(verifiedPassword);
     const userDTO = User.create(name, email, hashedPassword);
 
-    await this.userRepository.create(userDTO);
+    const createdUser = await this.userRepository.create(userDTO);
 
-    return;
+    const code = await this.verificationCodeRepository.create(createdUser.id);
+    await this.emailService.sendVerificationCode(
+      createdUser.name,
+      createdUser.email,
+      code
+    );
+
+    return {
+      status: "success",
+      message: "Usuário criado com sucesso",
+      data: null,
+    };
   }
 }
 
