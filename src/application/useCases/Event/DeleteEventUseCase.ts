8f6f630e6764fe947281b26515225fe8d31b9fc3
Marcos Vinicius Ferreira Pinto <181281132+MarcosFerreira2024@users.noreply.github.com>
2025-11-04 20:13:13 -0300

    Add DeleteEventUseCase for event deletion

---
+++ src/application/useCases/Event/DeleteEventUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "tsyringe";
+import { IEventRepository } from "../../../domain/interfaces/IEventRepository";
+import { UseCaseResponse } from "../../../infrastructure/types/global";
+
+@injectable()
+class DeleteEventUseCase {
+  constructor(
+    @inject("EventRepository") private eventRepository: IEventRepository
+  ) {}
+
+  async execute(eventId: string): Promise<UseCaseResponse> {
+    const existingEvent = await this.eventRepository.findById(eventId);
+
+    if (!existingEvent) {
+      throw new Error("Event not found.");
+    }
+
+    await this.eventRepository.delete(eventId);
+
+    return {
+      status: "success",
+      message: "Evento Deletado com sucesso",
+      data: null,
+    };
+  }
+}
+export { DeleteEventUseCase };
