d37160f786f2a40ee3e95940082edfbcfc912afd
Marcos Vinicius Ferreira Pinto <181281132+MarcosFerreira2024@users.noreply.github.com>
2025-11-04 20:18:04 -0300

    Update routes.ts

--- src/presentation/routes/routes.ts
+++ src/presentation/routes/routes.ts
@@ -1,15 +1,23 @@
 import { Router } from "express";
 import { user } from "./user";
 import { authentication } from "./auth";
+import { rating } from "./rating";
+import { outingRoutes } from "./outing";
+import { parkRoutes } from "./park";
+import { trailRoutes } from "./trail";
+import { eventRoutes } from "./event";
 
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../../infrastructure/libs/swagger/swagger-output.json";
-import { rating } from "./rating";
 
 const routes = Router();
 routes.use("/user", user);
 routes.use("/authentication", authentication);
 routes.use("/rating", rating);
+routes.use("/outing", outingRoutes);
+routes.use("/park", parkRoutes);
+routes.use("/trail", trailRoutes);
+routes.use("/event", eventRoutes);
 routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export { routes };
