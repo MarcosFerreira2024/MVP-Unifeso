e84c8f7559087ffb70c2d7065c736bf94114021c
Marcos Vinicius Ferreira Pinto <181281132+MarcosFerreira2024@users.noreply.github.com>
2025-11-04 20:13:57 -0300

    Add DeleteOutingUseCase for deleting outings

---
+++ src/application/useCases/Outing/DeleteOutingUseCase.ts
@@ -0,0 +1,22 @@
+import { inject, injectable } from "tsyringe";
+import { IOutingRepository } from "../../../domain/interfaces/IOutingRepository";
+
+@injectable()
+class DeleteOutingUseCase {
+  constructor(
+    @inject("OutingRepository")
+    private outingRepository: IOutingRepository
+  ) {}
+
+  async execute(id: string): Promise<void> {
+    const outingExists = await this.outingRepository.findById(id);
+
+    if (!outingExists) {
+      throw new Error("Passeio não encontrado");
+    }
+
+    await this.outingRepository.delete(id);
+  }
+}
+
+export { DeleteOutingUseCase };
