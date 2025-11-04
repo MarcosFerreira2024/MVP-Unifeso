5e03878c7a0a691205012f386d77c7639f198a3d
Marcos Vinicius Ferreira Pinto <181281132+MarcosFerreira2024@users.noreply.github.com>
2025-11-04 20:13:20 -0300

    Add FindEventByIdUseCase implementation

---
+++ src/application/useCases/Event/FindEventByIdUseCase.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from "tsyringe";
+import { IEventRepository } from "../../../domain/interfaces/IEventRepository";
+import { UseCaseResponse } from "../../../infrastructure/types/global";
+
+@injectable()
+class FindEventByIdUseCase {
+  constructor(
+    @inject("EventRepository") private eventRepository: IEventRepository
+  ) {}
+
+  async execute(eventId: string): Promise<UseCaseResponse> {
+    const eventFromDb = await this.eventRepository.findById(eventId);
+
+    return {
+      status: "success",
+      message: "Eventos Encontrados",
+      data: eventFromDb,
+    };
+  }
+}
+export { FindEventByIdUseCase };
