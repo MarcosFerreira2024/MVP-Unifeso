01192d47fddecc4508482b9a671193038e954495
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-12-04 16:59:06 -0300

    Update TokenProvider.ts

--- src/infrastructure/provider/TokenProvider.ts
+++ src/infrastructure/provider/TokenProvider.ts
@@ -1,29 +1,33 @@
 import jwt from "jsonwebtoken";
 import { injectable } from "tsyringe";
+import { ITokenProvider } from "../../domain/interfaces/ITokenProvider";
+import { Role } from "../../domain/interfaces/IUserRepository";
+import { randomUUID } from "crypto";
 
 @injectable()
 export class TokenProvider implements ITokenProvider {
   private secret = process.env.JWT_SECRET!;
 
   async generate(userId: string, email: string, role: string): Promise<string> {
-    return jwt.sign({ userId, email, role }, this.secret, {
+    const token = jwt.sign({ userId, email, role }, this.secret, {
       expiresIn: "24h",
+      jwtid: randomUUID(),
     });
+
+    return token;
   }
 
-  async verify(
-    token: string
-  ): Promise<{
+  async verify(token: string): Promise<{
     userId: string;
     email: string;
-    role: string;
+    role: Role;
     token: string;
   } | null> {
     try {
       const decoded = jwt.verify(token, this.secret) as {
         userId: string;
         email: string;
-        role: string;
+        role: Role;
       };
       return {
         userId: decoded.userId,
@@ -31,7 +35,7 @@ export class TokenProvider implements ITokenProvider {
         role: decoded.role,
         token,
       };
-    } catch {
+    } catch (e) {
       throw new Error("Token inválido ou expirado");
     }
   }
