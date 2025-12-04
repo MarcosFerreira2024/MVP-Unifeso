04b92348caf94acd7a07731c0c3fa9c94b1a47c7
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-12-04 17:00:50 -0300

    Update VerificationCodeService.ts

--- src/infrastructure/services/verificationCode/VerificationCodeService.ts
+++ src/infrastructure/services/verificationCode/VerificationCodeService.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { IVerificationCodeRepository } from "../../../domain/interfaces/IVerificationCodeRepository";
+import { IEmailService } from "../../../domain/interfaces/IEmailService";
 
 @injectable()
 class VerificationCodeService {
@@ -9,10 +10,14 @@ class VerificationCodeService {
     private verificationCodeRepository: IVerificationCodeRepository
   ) {}
 
-  public async createAndSendEmail(userId: string, email: string) {
+  public async createAndSendEmail(
+    userId: string,
+    email: string,
+    userName: string
+  ) {
     const code = await this.verificationCodeRepository.create(userId);
 
-    await this.emailService.sendVerificationCode(userId, email, code);
+    await this.emailService.sendVerificationCode(userName, email, code);
   }
 }
 
